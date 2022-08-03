610b7c8266c235a1f8903073e7427c961795521f
acclrtrrr <andrey.novikov2001@yandex.ru>
2022-08-03 15:07:59 +0300

    The first version of the project has been completed.

--- artstore/artstore/settings.py
+++ artstore/artstore/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'art_store',
-        'USER': 'postgres',
-        'PASSWORD': '1656_1656',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
